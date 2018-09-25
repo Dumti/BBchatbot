874ef0be55767c65b0bc3a8aee6e94e513fba6e9
Dumti <candy2land@naver.com>
2018-09-25 22:10:30 +0900

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,8 +1,22 @@
 
     <?php
-    echo '
-    {
-    "type": "buttons",
-    "buttons": ["메뉴1", "메뉴2", "메뉴3"}
-    }'
-    ?>
+$data = json_decode(file_get_contents('php://input'), true);
+$content = $data["content"];
+
+switch($content)
+{
+        case "Menu1"
+            echo '
+            {
+            "message":
+            {
+            "text": "You chose Menu 1."
+            },
+            "keyboard":
+            {
+            "type": "buttons",
+            "buttons": ["Menu1", "Menu2", "Menu3"]
+            }
+            }'
+            break;
+        ?>
